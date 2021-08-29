b08fb4d41df30eeab57d517e7a88d39c255ce853
qqlcx5 <qqlcx5@163.com>
2021-08-29 19:07:21 +0800

    🚑  git husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 }
