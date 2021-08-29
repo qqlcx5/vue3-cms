c2893db4fd3956a419fb48e0c3f9f367e4724dd1
qqlcx5 <qqlcx5@163.com>
2021-08-29 18:21:39 +0800

    ✨ element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import registerApp from './global'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+const app = createApp(App)
 
-createApp(App).use(store).use(router).mount('#app')
+// registerApp(app)
+app.use(ElementPlus)
+app.use(store).use(router).mount('#app')
