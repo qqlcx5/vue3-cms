6e1a9adecfbc6d244bdc0f5b806a06984e196de8
qqlcx5 <qqlcx5@163.com>
2021-08-29 18:49:17 +0800

    🚀

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <template>
-  <div id="app">
-    <router-link to="/login">登录</router-link>
-    <router-link to="/main">首页</router-link>
-    <router-view></router-view>
-
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view />
 </template>
 
 <style lang="less">
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import registerApp from './global'
-import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
-const app = createApp(App)
 
-// registerApp(app)
-app.use(ElementPlus)
-app.use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
