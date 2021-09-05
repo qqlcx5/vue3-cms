746a63ca50ce8ee85ad7d9d3291c9b1131a7a5c1
qqlcx5 <qqlcx5@163.com>
2021-09-06 00:03:26 +0800

    ✨  登录页面和逻辑

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize.css'
+import './assets/css/index.less'
+import hyRequest from './service'
+
 // element Plus组件报错
 // import registerApp from './global/index'
 // 注册全局方法
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
-import hyRequest from './service'
 
 // registerApp(app)
 const app = createApp(App)
---
+++ src/views/login/cpns/login-account.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login-account">
+    <el-form label-width="60px" :rules="rules" :model="account" ref="formRef">
+      <el-form-item label="账号" prop="name">
+        <el-input v-model="account.name" />
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="account.password" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref } from 'vue'
+import { ElForm } from 'element-plus'
+
+import { rules } from '../config/account-config'
+
+export default defineComponent({
+  setup() {
+    const account = reactive({
+      name: '',
+      password: '',
+    })
+    const formRef = ref<InstanceType<typeof ElForm>>()
+    const loginAction = () => {
+      formRef.value?.validate((valid) => {
+        if (valid) {
+          console.log('loginAction')
+        }
+      })
+    }
+    return {
+      rules,
+      account,
+      formRef,
+      loginAction,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
